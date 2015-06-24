c8a7c2d7a26fe686e049fb66f1c95d40127bb149
Alexey Korolev <korolev2009@gmail.com>
2015-06-24 09:12:22 +0300

    Finished

--- app/src/main/java/net/alexandroid/sunshine/MainActivity.java
+++ app/src/main/java/net/alexandroid/sunshine/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -53,25 +52,12 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             case R.id.action_settings:
                 startActivity(new Intent(MainActivity.this, SettingsActivity.class));
                 break;
-            case R.id.action_map:
-                openPreferredLocation();
-                break;
         }
 
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void openPreferredLocation() {
-        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", mLocation).build();
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geoLocation);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            Log.d(LOG_TAG, "Couldn't call " + mLocation + ", no receiving apps installed!");
-        }
-    }
 
     @Override
     protected void onResume() {
